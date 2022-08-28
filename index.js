826c7769a363ad919260969daeee441b7b88ad06
Ritvik Mathur <ritvikmathur1999@gmail.com>
2022-08-28 21:59:53 +0530

    repo created

---
+++ index.js
@@ -0,0 +1,20 @@
+//bpmn filename
+const bpmnFilePath = "decision tree2.bpmn";
+
+//BpmnJS configuration
+const bpmnConfig = {
+  container: document.getElementById("js-canvas"),
+  height: 900,
+};
+
+//BpmnJS initialization
+var viewer = new BpmnJS(bpmnConfig);
+
+//file processing
+fetch(bpmnFilePath)
+  .then((response) => response.text())
+  .then((data) => {
+    const dataXML = data;
+    viewer.importXML(dataXML);
+    viewer.get("canvas").zoom("fit-viewport");
+  });
